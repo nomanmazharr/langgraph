0816cf513c06cb2bfdc0c591f5a65acd3bcf620a
Noman <nmazhar911@gmail.com>
2025-08-27 05:37:15 +0500

    implement chat threading for user to open different sessions/chat like gpt

---
+++ chatbot_streamlit_integrated/chatbot_threading.py
@@ -0,0 +1,97 @@
+import streamlit as st
+from langgraph_backend import workflow
+from langchain_core.messages import HumanMessage,AIMessage
+import uuid
+
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
+    add_thread(st.session_state['thread_id'])
+    st.session_state["message_history"] = []
+
+def load_conversation(thread_id):
+    state = workflow.get_state(config = {'configurable': {'thread_id': thread_id}})
+    return state.values.get('messages', [])
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = []
+
+
+add_thread(st.session_state['thread_id'])
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+
+st.sidebar.title('Langgraph Bot')
+if st.sidebar.button('New Chat'):
+    reset_chat()
+
+
+st.sidebar.header('My Conversations')
+
+
+for thread_id in st.session_state['chat_threads'][::-1]:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = load_conversation(thread_id)
+
+        temp_messages = []
+
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role = 'user'
+            else:
+                role = 'assistant'
+
+            temp_messages.append({'role': role, 'content': msg.content})
+        
+        st.session_state['message_history'] = temp_messages
+
+
+for messages in st.session_state['message_history']:
+    with st.chat_message(messages['role']):
+        st.text(messages['content'])
+
+user_input = st.chat_input('Type here')
+
+CONFIG = {"configurable": {'thread_id': st.session_state['thread_id']}}
+
+if user_input:
+
+    st.session_state['message_history'].append({'role': 'user', 
+                                                'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    with st.chat_message('assistant'):
+        def ai_only_stream():
+            for message_chunk, metadata in workflow.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config = CONFIG,
+                stream_mode= 'messages'
+            ):
+                if isinstance(message_chunk, AIMessage):
+                    yield message_chunk.content
+
+        ai_message = st.write_stream(ai_only_stream())
+
+
+    st.session_state['message_history'].append({'role': 'assistant', 
+                                                'content': ai_message})
