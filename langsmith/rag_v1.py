f8aa36e74079f9d9f86ed34db4f6f5db421db195
Noman <nmazhar911@gmail.com>
2025-08-30 19:37:51 +0500

    tesing rag application flow with langsmith

---
+++ langsmith/rag_v1.py
@@ -0,0 +1,55 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_anthropic import ChatAnthropic
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()  # expects OPENAI_API_KEY in .env
+os.environ['LANGCHAIN_PROJECT'] = 'RAG'
+PDF_PATH = "langsmith\islr.pdf"  
+
+# 1) Load PDF
+loader = PyPDFLoader(PDF_PATH)
+docs = loader.load()  # one Document per page
+
+# 2) Chunk
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+splits = splitter.split_documents(docs)
+
+# 3) Embed + index
+
+emb = MistralAIEmbeddings(
+    model="mistral-embed",
+    api_key="JgkR1k8M86MlnwMOe89NZxmfgMaVinWo")
+
+# emb = OpenAIEmbeddings(model="text-embedding-3-small")
+vs = FAISS.from_documents(splits, emb)
+retriever = vs.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+
+# 4) Prompt
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "Answer ONLY from the provided context. If not found, say you don't know."),
+    ("human", "Question: {question}\n\nContext:\n{context}")
+])
+
+# 5) Chain
+llm = ChatAnthropic(model='claude-3-5-sonnet-20241022')
+def format_docs(docs): return "\n\n".join(d.page_content for d in docs)
+
+parallel = RunnableParallel({
+    "context": retriever | RunnableLambda(format_docs),
+    "question": RunnablePassthrough()
+})
+
+chain = parallel | prompt | llm | StrOutputParser()
+
+# 6) Ask questions
+print("PDF RAG ready. Ask a question (or Ctrl+C to exit).")
+q = input("\nQ: ")
+ans = chain.invoke(q.strip())
+print("\nA:", ans)
